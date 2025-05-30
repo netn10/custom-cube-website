053433887d7507a557b459cf79f35f3371fd1552
Netanel Aker <netn10@gmail.com>
2025-05-31 01:56:18 +0300

    Deployment Sat 05/31/2025  1:56:18.80

--- deploy.bat
+++ deploy.bat
@@ -1,27 +1,14 @@
 @echo off
-setlocal enabledelayedexpansion
-
 echo Deploying Frontend...
 git add .
 git commit -m "Deployment %date% %time%"
-git push origin master
+git push heroku master
 
 echo Deploying Backend...
 cd backend
 git add .
 git commit -m "Backend Deployment %date% %time%"
-git push origin master
-
-echo.
-echo ========================================
-echo Backend code pushed to GitHub.
-echo To complete the backend deployment on Render:
-echo 1. Go to your Render Dashboard
-echo 2. Select the 'mtgcube-api' service
-echo 3. Click on 'Manual Deploy' -> 'Deploy latest commit'
-echo ========================================
-echo.
-
+git push heroku master
 cd ..
-echo Frontend deployment to GitHub complete!
-echo Backend code pushed to GitHub. Follow the steps above to deploy to Render.
+
+echo Deployment Complete!
