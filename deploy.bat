c312058ceda19f094896bb0d1b2e5a6ea7af2dfd
Netanel Aker <netn10@gmail.com>
2025-05-31 00:38:32 +0300

    Remove large PDF file from Git tracking

--- deploy.bat
+++ deploy.bat
@@ -2,13 +2,13 @@
 echo Deploying Frontend...
 git add .
 git commit -m "Deployment %date% %time%"
-git push heroku master
+git push origin master
 
 echo Deploying Backend...
 cd backend
 git add .
 git commit -m "Backend Deployment %date% %time%"
-git push heroku master
+git push origin master
 cd ..
 
 echo Deployment Complete!
