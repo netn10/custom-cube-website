3851ab93802ed53bb7b420bcbfa5f7f1051fff77
netn10 <netn1@example.com>
2025-06-13 13:58:29 +0300

    Deployment Fri 06/13/2025 13:58:29.69

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,16 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo Deploying Frontend...
+echo Pushing to GitHub Repository...
 git add .
 git commit -m "Deployment %date% %time%"
-git pull heroku master --rebase
-git push heroku master
+git pull origin master --rebase
+git push origin master
 if errorlevel 1 (
-    echo Error deploying frontend. Please resolve any conflicts and try again.
+    echo Error pushing to GitHub. Please resolve any conflicts and try again.
     exit /b 1
 )
+
+echo Successfully pushed to GitHub!
+pause
 
